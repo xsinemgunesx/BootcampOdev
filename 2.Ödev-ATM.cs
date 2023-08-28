3e57d2f563c188b6a423552e4a8be6ae394123c9
xsinemgunesx <100774851+xsinemgunesx@users.noreply.github.com>
2023-08-28 23:50:56 +0300

    3.ödev

--- 2.Ödev-ATM.cs
+++ 2.Ödev-ATM.cs
@@ -1,67 +1,67 @@
-using System;
+//using System;
 
-public abstract class ATM
-{
-    private int bakiye;
-    public int Bakiye
-    {
-        get { return bakiye; }
-        set { bakiye = value; }
-    }
-    public abstract string paraCek(int x);
-    public abstract int paraYatir(int x);
+//public abstract class ATM
+//{
+//    private int bakiye;
+//    public int Bakiye
+//    {
+//        get { return bakiye; }
+//        set { bakiye = value; }
+//    }
+//    public abstract string paraCek(int x);
+//    public abstract int paraYatir(int x);
 
-    public abstract string gosterBakiye();
-}
-class Islem : ATM
-{
+//    public abstract string gosterBakiye();
+//}
+//class Islem : ATM
+//{
 
-    public override string paraCek(int money)
-    {
-        if (Bakiye >= money) { Bakiye -= money; return ("Kalan bakiye : " + Bakiye); }
-        else { return ("Yeterli bakiye yok."); }
+//    public override string paraCek(int money)
+//    {
+//        if (Bakiye >= money) { Bakiye -= money; return ("Kalan bakiye : " + Bakiye); }
+//        else { return ("Yeterli bakiye yok."); }
 
-    }
-    public override int paraYatir(int money)
-    {
-        Bakiye += money;
-        return Bakiye;
-    }
-    public override string gosterBakiye()
-    {
-        return ("Bakiyeniz : " + Bakiye);
-    }
-}
-class Show
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("1.Bakiye Görüntüle\n2.Para çekme\n3.Para yatirma");
-        Console.WriteLine("Yapmak istediðiniz iþlemi seçiniz..");
-        int sec = Convert.ToInt32(Console.ReadLine());
+//    }
+//    public override int paraYatir(int money)
+//    {
+//        Bakiye += money;
+//        return Bakiye;
+//    }
+//    public override string gosterBakiye()
+//    {
+//        return ("Bakiyeniz : " + Bakiye);
+//    }
+//}
+//class Show
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("1.Bakiye Görüntüle\n2.Para çekme\n3.Para yatirma");
+//        Console.WriteLine("Yapmak istediðiniz iþlemi seçiniz..");
+//        int sec = Convert.ToInt32(Console.ReadLine());
 
-        ATM islem = new Islem();
-        islem.Bakiye = 2500;
-        switch (sec)
-        {
-            case 1: Console.WriteLine(islem.gosterBakiye()); break;
-            case 2:
-                {
-                    Console.WriteLine("Cekmek istediðiniz tutari giriniz.");
-                    int tutar = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine(islem.paraCek(tutar));
-                    break;
-                }
-            case 3:
-                {
-                    Console.WriteLine("Yatirmak istediðiniz tutari giriniz.");
-                    int tutar = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine(islem.paraYatir(tutar));
-                    break;
-                }
-            default:
-                Console.WriteLine("Ýþleminiz iptal edildi.");
-                break;
-        }
-    }
-}
+//        ATM islem = new Islem();
+//        islem.Bakiye = 2500;
+//        switch (sec)
+//        {
+//            case 1: Console.WriteLine(islem.gosterBakiye()); break;
+//            case 2:
+//                {
+//                    Console.WriteLine("Cekmek istediðiniz tutari giriniz.");
+//                    int tutar = Convert.ToInt32(Console.ReadLine());
+//                    Console.WriteLine(islem.paraCek(tutar));
+//                    break;
+//                }
+//            case 3:
+//                {
+//                    Console.WriteLine("Yatirmak istediðiniz tutari giriniz.");
+//                    int tutar = Convert.ToInt32(Console.ReadLine());
+//                    Console.WriteLine(islem.paraYatir(tutar));
+//                    break;
+//                }
+//            default:
+//                Console.WriteLine("Ýþleminiz iptal edildi.");
+//                break;
+//        }
+//    }
+//}
---
+++ 3.Ödev-Zoo.cs
@@ -0,0 +1,92 @@
+﻿namespace BootcampOdev;
+
+public abstract class Animal
+{
+	private string name;
+	private string kind;
+	private int age;
+	public string Name
+	{
+		get { return name; }
+		set { name = value; }
+	}
+	public string Kind
+	{
+		get { return kind; }
+		set { kind = value; }
+	}
+	public int Age
+	{
+		get { return age; }
+		set { age = value; }
+	}
+	public abstract void makeNoise();
+	public void zooland()
+	{
+		Console.WriteLine($"Hayvanin ismi :{Name},\nHayvanin turu : {Kind},\nHayvanin yasi : {Age} ");
+	}
+}
+
+public class Dog : Animal
+{
+	public Dog()
+	{
+		Name = "Köpüş";
+		Kind = "Köpek";
+		Age = 4;
+	}
+	public override void makeNoise()
+	{
+		Console.WriteLine("HAV HAV HAV!!!");
+	}
+	
+}
+public class Cat : Animal
+{
+	public Cat()
+	{
+		Name = "Miya";
+		Kind = "Kedi";
+		Age = 2;
+	}
+	public override void makeNoise()
+	{
+		Console.WriteLine("Miyav miyav!!!");
+	}
+}
+public class Bird : Animal
+{
+	public Bird()
+	{
+		Name = "Mavis";
+		Kind = "Kus";
+		Age = 1;
+	}
+
+	public override void makeNoise()
+	{
+		Console.WriteLine("Cik cik cik!!!");
+	}
+}
+public class ZOO
+{
+	static void showAnimal(Animal[] animal)
+	{
+		for(int i = 0;i < animal.Length; i++)
+		{
+			animal[i].zooland();
+			animal[i].makeNoise();
+			Console.WriteLine();
+		}
+
+	}
+	static void Main(String[] args)
+	{
+		Animal[] animalList = new Animal[3];
+		animalList[0] = new Dog();
+		animalList[1] = new Cat();
+		animalList[2] = new Bird();
+		showAnimal(animalList);
+		
+	}
+}
